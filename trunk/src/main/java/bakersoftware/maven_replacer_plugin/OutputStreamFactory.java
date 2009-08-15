34e1c1c8b0c3bace6a5767e3a2ff80cc87965fbb
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-08-15 12:13:13 +0000

    new feature to output to given file (if provided) and fixed regex defect by adding param to specify if token contains regex

--- trunk/src/main/java/bakersoftware/maven_replacer_plugin/OutputStreamFactory.java
+++ trunk/src/main/java/bakersoftware/maven_replacer_plugin/OutputStreamFactory.java
@@ -3,7 +3,5 @@ package bakersoftware.maven_replacer_plugin;
 import java.io.OutputStream;
 
 public interface OutputStreamFactory {
-
 	OutputStream newStream();
-
 }
--- trunk/src/main/java/bakersoftware/maven_replacer_plugin/TokenReplacer.java
+++ trunk/src/main/java/bakersoftware/maven_replacer_plugin/TokenReplacer.java
@@ -15,28 +15,51 @@ public class TokenReplacer {
 		this.lineSeparator = lineSeparator;
 		this.streamFactory = streamFactory;
 	}
-	
-	public void replaceTokens(String token, String value) throws IOException {
+
+	public void replaceTokens(String token, String value, boolean isTokenRegex)
+			throws IOException {
 		StringBuffer buffer = new StringBuffer();
-		
+
 		InputStream inputStream = streamFactory.getNewInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				inputStream));
+
 		String line = reader.readLine();
 		if (line == null) {
 			throw new IOException("Could not read from stream");
 		}
 		while (line != null) {
-			buffer.append(line.replaceAll(token, value));
+			if (isTokenRegex) {
+				buffer.append(line.replaceAll(token, value));
+			} else {
+				buffer.append(replaceNonRegex(line, token, value));
+			}
 			if (lineSeparator != null) {
 				buffer.append(lineSeparator);
 			}
 			line = reader.readLine();
 		}
 		reader.close();
-		
+
 		Writer writer = new OutputStreamWriter(streamFactory.getNewOutputStream());
 		writer.write(buffer.toString());
 		writer.close();
 	}
+
+	private String replaceNonRegex(String input, String token, String value) {
+		if (input.length() == 0) {
+			return input;
+		}
+
+		StringBuffer buffer = new StringBuffer();
+		int start = 0;
+		int end = 0;
+		while ((end = input.indexOf(token, start)) >= 0) {
+			buffer.append(input.substring(start, end));
+			buffer.append(value);
+			start = end + token.length();
+		}
+		return buffer.append(input.substring(start)).toString();
+	}
+
 }
