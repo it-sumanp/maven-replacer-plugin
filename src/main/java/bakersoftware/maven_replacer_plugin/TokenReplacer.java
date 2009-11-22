3466c318ceeaa68738ea2a6b9775d67d601fa976
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-11-22 22:08:29 +0000

    major refactoring after a spike to back out some bad design for multiple replacements. the replacer context took on too much and so did token replacer. unfortunately the unit tests became largely obsolete and need rewriting.

--- src/main/java/bakersoftware/maven_replacer_plugin/TokenReplacer.java
+++ src/main/java/bakersoftware/maven_replacer_plugin/TokenReplacer.java
@@ -1,44 +1,7 @@
 package bakersoftware.maven_replacer_plugin;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-
-import bakersoftware.maven_replacer_plugin.file.StreamFactory;
-
 public class TokenReplacer {
-	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
-
-	public void replaceTokens(String token, String value, boolean isTokenRegex,
-			StreamFactory streamFactory) throws IOException {
-		StringBuffer buffer = readContents(streamFactory.getNewInputStream());
-		String result = replaceContents(buffer.toString(), token, value, isTokenRegex);
-
-		Writer writer = new OutputStreamWriter(streamFactory.getNewOutputStream());
-		writer.write(result);
-		writer.close();
-	}
-
-	private StringBuffer readContents(InputStream inputStream) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-		StringBuffer buffer = new StringBuffer();
-		String line = reader.readLine();
-		if (line == null) {
-			throw new IOException("Could not read from stream");
-		}
-		while (line != null) {
-			buffer.append(line).append(LINE_SEPARATOR);
-			line = reader.readLine();
-		}
-		reader.close();
-		return buffer;
-	}
-
-	private String replaceContents(String contents, String token, String value, boolean isTokenRegex) {
+	public String replaceContents(String contents, String token, String value, boolean isTokenRegex) {
 		String valueToReplaceWith = value == null ? "" : value;
 		final String result;
 		if (isTokenRegex) {
