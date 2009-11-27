760bc06d8d5e0fa2a95514632067cedd7b12b730
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-11-27 11:19:54 +0000

    Token replacer unit test

---
+++ trunk/src/test/java/bakersoftware/maven_replacer_plugin/TokenReplacerTest.java
@@ -0,0 +1,39 @@
+package bakersoftware.maven_replacer_plugin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokenReplacerTest {
+	private TokenReplacer replacer;
+
+	@Before
+	public void setUp() {
+		replacer = new TokenReplacer();
+	}
+
+	@Test
+	public void shouldReplaceNonRegexTokenWithValue() throws Exception {
+		String results = replacer.replaceContents("some $token$", "$token$", "value", false);
+		assertEquals(results, "some value");
+	}
+
+	@Test
+	public void shouldReplaceRegexTokenWithValue() throws Exception {
+		String results = replacer.replaceContents("some token", "t.k.n", "value", true);
+		assertEquals("some value", results);
+	}
+
+	@Test
+	public void shouldReplaceTokenWithEmptyValue() throws Exception {
+		String results = replacer.replaceContents("some token", "t.k.n", null, true);
+		assertEquals("some ", results);
+	}
+
+	@Test
+	public void shouldReplaceTokenInMulipleLines() throws Exception {
+		String results = replacer.replaceContents("some\ntoken", "t.k.n", null, true);
+		assertEquals("some\n", results);
+	}
+}
