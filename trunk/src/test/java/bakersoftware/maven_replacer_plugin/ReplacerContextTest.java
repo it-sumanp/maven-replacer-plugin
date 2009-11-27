41289f2a7d60d42b6c8e7d4e9aa16ef27fe13636
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-11-27 11:09:21 +0000

    ReplacerContext test

---
+++ trunk/src/test/java/bakersoftware/maven_replacer_plugin/ReplacerContextTest.java
@@ -0,0 +1,49 @@
+package bakersoftware.maven_replacer_plugin;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import bakersoftware.maven_replacer_plugin.file.FileUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReplacerContextTest {
+	private static final String FILE = "some file";
+	private static final String VALUE = "value";
+	private static final String TOKEN = "token";
+	@Mock
+	private FileUtils fileUtils;
+
+	@Test
+	public void shouldReturnGivenParameters() throws Exception {
+		ReplacerContext context = new ReplacerContext(fileUtils, TOKEN, VALUE);
+		assertEquals(TOKEN, context.getToken());
+		assertEquals(VALUE, context.getValue());
+		verifyZeroInteractions(fileUtils);
+	}
+
+	@Test
+	public void shouldUseTokenFromFileUtilsIfGivenTokenIsNull() throws Exception {
+		when(fileUtils.readFile(FILE)).thenReturn(TOKEN);
+
+		ReplacerContext context = new ReplacerContext(fileUtils, null, VALUE);
+		context.setTokenFile(FILE);
+		assertEquals(TOKEN, context.getToken());
+		assertEquals(VALUE, context.getValue());
+	}
+
+	@Test
+	public void shouldUseValueFromFileUtilsIfGivenValueIsNull() throws Exception {
+		when(fileUtils.readFile(FILE)).thenReturn(VALUE);
+
+		ReplacerContext context = new ReplacerContext(fileUtils, TOKEN, null);
+		context.setValueFile(FILE);
+		assertEquals(TOKEN, context.getToken());
+		assertEquals(VALUE, context.getValue());
+	}
+}
