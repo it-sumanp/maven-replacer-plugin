2026af5cade439ddd5289bcb6e7ad61d8e2137c1
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-12-20 09:06:55 +0000

    adding ant style include/exclude for multiple file replacement. currently requires ant on dependency unless using with dep jar

---
+++ trunk/src/main/java/bakersoftware/maven_replacer_plugin/include/FileSelector.java
@@ -0,0 +1,34 @@
+package bakersoftware.maven_replacer_plugin.include;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.tools.ant.DirectoryScanner;
+
+public class FileSelector {
+
+	public List<String> listIncludes(List<String> includes, List<String> excludes) {
+		if (includes == null || includes.isEmpty()) {
+			return Collections.emptyList();
+		}
+
+		DirectoryScanner directoryScanner = new DirectoryScanner();
+		directoryScanner.addDefaultExcludes();
+		directoryScanner.setBasedir(new File("."));
+		directoryScanner.setIncludes(stringListToArray(includes));
+		directoryScanner.setExcludes(stringListToArray(excludes));
+
+		directoryScanner.scan();
+		return Arrays.asList(directoryScanner.getIncludedFiles());
+	}
+
+	private String[] stringListToArray(List<String> stringList) {
+		if (stringList == null) {
+			return null;
+		}
+		return stringList.toArray(new String[] {});
+	}
+}
