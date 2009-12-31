970db78dd2c4fbccac55d702846be07251bd40c1
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2009-12-31 05:31:15 +0000

    adding all regex pattern flags

--- src/main/java/bakersoftware/maven_replacer_plugin/PatternFlagsFactory.java
+++ src/main/java/bakersoftware/maven_replacer_plugin/PatternFlagsFactory.java
@@ -4,13 +4,53 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class PatternFlagsFactory {
-
+	public static final int NO_FLAGS = -1;
 
 	public int buildFlags(List<String> flags) {
 		if (flags == null || flags.isEmpty()) {
-			return Pattern.MULTILINE; 
+			return NO_FLAGS;
 		}
-		// TODO Auto-generated method stub
-		return 0;
+
+		int value = 0;
+		for (String flag : flags) {
+			value |= getValueOf(flag);
+		}
+		return value;
+	}
+
+	private int getValueOf(String flag) {
+		if ("CANON_EQ".equalsIgnoreCase(flag)) {
+			return Pattern.CANON_EQ;
+		}
+
+		if ("CASE_INSENSITIVE".equalsIgnoreCase(flag)) {
+			return Pattern.CASE_INSENSITIVE;
+		}
+
+		if ("COMMENTS".equalsIgnoreCase(flag)) {
+			return Pattern.COMMENTS;
+		}
+
+		if ("DOTALL".equalsIgnoreCase(flag)) {
+			return Pattern.DOTALL;
+		}
+
+		if ("LITERAL".equalsIgnoreCase(flag)) {
+			return Pattern.LITERAL;
+		}
+
+		if ("MULTILINE".equalsIgnoreCase(flag)) {
+			return Pattern.MULTILINE;
+		}
+
+		if ("UNICODE_CASE".equalsIgnoreCase(flag)) {
+			return Pattern.UNICODE_CASE;
+		}
+
+		if ("UNIX_LINES".equalsIgnoreCase(flag)) {
+			return Pattern.UNIX_LINES;
+		}
+
+		throw new IllegalArgumentException("Unknown regex flag: " + flag);
 	}
 }
--- src/test/java/bakersoftware/maven_replacer_plugin/PatternFlagsFactoryTest.java
+++ src/test/java/bakersoftware/maven_replacer_plugin/PatternFlagsFactoryTest.java
@@ -1,24 +1,59 @@
 package bakersoftware.maven_replacer_plugin;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class PatternFlagsFactoryTest {
+	private List<String> inputFlags;
+	private int expectedFlags;
+
 	private PatternFlagsFactory factory;
+
+	public PatternFlagsFactoryTest(List<String> inputFlags, int expectedFlags) {
+		this.inputFlags = inputFlags;
+		this.expectedFlags = expectedFlags;
+	}
+
+	@Parameters
+	public static Collection<Object[]> params() {
+		return asList(new Object[][] {
+				{ null, -1 },
+				{ asList(), -1 },
+				{ asList("CANON_EQ"), Pattern.CANON_EQ },
+				{ asList("CASE_INSENSITIVE"), Pattern.CASE_INSENSITIVE },
+				{ asList("COMMENTS"), Pattern.COMMENTS },
+				{ asList("DOTALL"), Pattern.DOTALL },
+				{ asList("LITERAL"), Pattern.LITERAL },
+				{ asList("MULTILINE"), Pattern.MULTILINE },
+				{ asList("UNICODE_CASE"), Pattern.UNICODE_CASE },
+				{ asList("UNIX_LINES"), Pattern.UNIX_LINES },
+				{ asList("CANON_EQ", "CASE_INSENSITIVE"),
+						Pattern.CANON_EQ | Pattern.CASE_INSENSITIVE } });
+	}
 
 	@Before
 	public void setUp() {
 		factory = new PatternFlagsFactory();
 	}
-	
+
 	@Test
-	public void shouldReturnMultiline() throws Exception {
-		assertEquals(Pattern.MULTILINE, factory.buildFlags(null));
-		assertEquals(Pattern.MULTILINE, factory.buildFlags(new ArrayList<String>()));
+	public void shouldReturnBitValueForFlags() throws Exception {
+		assertEquals(expectedFlags, factory.buildFlags(inputFlags));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionWhenFlagIsInvalid() throws Exception {
+		factory.buildFlags(asList("invalid"));
 	}
 }
