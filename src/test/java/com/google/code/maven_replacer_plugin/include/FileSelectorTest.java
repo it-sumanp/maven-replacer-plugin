ed4daafe8f86a9ef04ede9f9c0b1a0af529b6f8d
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2010-04-03 00:34:01 +0000

    can now specify basedir (Issue 31)

--- src/test/java/com/google/code/maven_replacer_plugin/include/FileSelectorTest.java
+++ src/test/java/com/google/code/maven_replacer_plugin/include/FileSelectorTest.java
@@ -1,23 +1,20 @@
 package com.google.code.maven_replacer_plugin.include;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.util.List;
 
 import org.junit.Test;
 
 public class FileSelectorTest {
-	private static final String PATH_SEPARATOR = File.separator;
-	
 	@Test
 	public void shouldReturnMultipleFilesToInclude() {
 		FileSelector selector = new FileSelector();
-		List<String> files = selector.listIncludes(asList("test/include1", "test/file*"), asList("test/file3"));
+		List<String> files = selector.listIncludes("test", asList("include1", "file*"), asList("file3"));
 		assertEquals(3, files.size());
-		assertEquals("test" + PATH_SEPARATOR + "file1", files.get(0));
-		assertEquals("test" + PATH_SEPARATOR + "file2", files.get(1));
-		assertEquals("test" + PATH_SEPARATOR + "include1", files.get(2));
+		assertEquals("file1", files.get(0));
+		assertEquals("file2", files.get(1));
+		assertEquals("include1", files.get(2));
 	}
 }
