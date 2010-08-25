900ae2d14256ecd5f78e8f073e656031e4dd9828
baker.steven.83@gmail.com <baker.steven.83@gmail.com@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2010-08-25 13:06:55 +0000

    using commons file io to write file contents. removed unnessicary file folder creations (done in FileUtils write)

--- trunk/src/main/java/com/google/code/maven_replacer_plugin/file/FileUtils.java
+++ trunk/src/main/java/com/google/code/maven_replacer_plugin/file/FileUtils.java
@@ -1,10 +1,7 @@
 package com.google.code.maven_replacer_plugin.file;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 public class FileUtils {
 	public boolean fileNotExists(String filename) {
@@ -33,9 +30,6 @@ public class FileUtils {
 
 	public void writeToFile(String outputFile, String content) throws IOException {
 		ensureFolderStructureExists(outputFile);
-		
-		Writer writer = new OutputStreamWriter(new FileOutputStream(outputFile));
-		writer.write(content);
-		writer.close();
+		org.apache.commons.io.FileUtils.writeStringToFile(new File(outputFile), content);
 	}
 }
