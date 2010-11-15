2c58a54714878dc8834747c49c781d92c59d0d39
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2010-11-15 04:19:46 +0000

    adding preserve dir for use with outputDir. adding outputBasedir which can be used when it needs to be different from the basedir (perhaps the execution path). split out outputfilename logic into own class

---
+++ src/test/java/com/google/code/maven_replacer_plugin/OutputFilenameBuilderTest.java
@@ -0,0 +1,81 @@
+package com.google.code.maven_replacer_plugin;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.code.maven_replacer_plugin.file.FileUtils;
+
+public class OutputFilenameBuilderTest {
+	private static final String INPUT_FILE = "parent/input";
+	private static final String BASE_DIR = ".";
+	private static final String OUTPUT_DIR = "target/out";
+	private static final String OUTPUT_FILE = "outputFile";
+	private static final String OUTPUT_BASE_DIR = "outputBaseDir";
+	
+	private OutputFilenameBuilder builder;
+	private ReplacerMojo mojo;
+	private FileUtils fileUtils;
+
+	@Before
+	public void setUp() {
+		mojo = mock(ReplacerMojo.class);
+		when(mojo.getBasedir()).thenReturn(BASE_DIR);
+		
+		fileUtils = new FileUtils();
+		builder = new OutputFilenameBuilder();
+	}
+	
+	@Test
+	public void shouldPrefixBasedirWhenNotPreservingPath() {
+		when(mojo.isPreserveDir()).thenReturn(false);
+		when(mojo.getOutputDir()).thenReturn(OUTPUT_DIR);
+		
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(BASE_DIR, OUTPUT_DIR, "input")));
+	}
+	
+	@Test
+	public void shouldPreservePathWhenPreserveIsEnabled() {
+		when(mojo.isPreserveDir()).thenReturn(true);
+		when(mojo.getOutputDir()).thenReturn(OUTPUT_DIR);
+		
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(BASE_DIR, OUTPUT_DIR, INPUT_FILE)));
+	}
+	
+	@Test
+	public void shouldPrefixBasedirWhenNotUsingOutputBasedir() {
+		when(mojo.getOutputDir()).thenReturn(OUTPUT_DIR);
+		
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(BASE_DIR, OUTPUT_DIR, "input")));
+	}
+	
+	@Test
+	public void shouldPrefixWithOutputBasedirWhenUsingOutputBasedir() {
+		when(mojo.getOutputBasedir()).thenReturn(OUTPUT_BASE_DIR);
+		when(mojo.getOutputDir()).thenReturn(OUTPUT_DIR);
+		
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(OUTPUT_BASE_DIR, OUTPUT_DIR, "input")));
+	}
+	
+	@Test
+	public void shouldReturnInputFileWithBaseDirWhenNoOutputDirOrNoOutputFile() {
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(BASE_DIR, INPUT_FILE)));
+	}
+	
+	@Test
+	public void shouldWriteToOutputFileWhenNotUsingOutputDirAndIsSet() {
+		when(mojo.getOutputFile()).thenReturn(OUTPUT_FILE);
+		
+		String output = builder.buildFrom(INPUT_FILE, mojo);
+		assertThat(output, equalTo(fileUtils.createFullPath(BASE_DIR, OUTPUT_FILE)));
+	}
+}
