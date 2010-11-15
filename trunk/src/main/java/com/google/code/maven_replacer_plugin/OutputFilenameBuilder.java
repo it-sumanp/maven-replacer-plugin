225075e5c41d0ee2f3680c4060ef44badab342ed
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2010-11-15 04:19:46 +0000

    adding preserve dir for use with outputDir. adding outputBasedir which can be used when it needs to be different from the basedir (perhaps the execution path). split out outputfilename logic into own class

---
+++ trunk/src/main/java/com/google/code/maven_replacer_plugin/OutputFilenameBuilder.java
@@ -0,0 +1,34 @@
+package com.google.code.maven_replacer_plugin;
+
+import java.io.File;
+
+import com.google.code.maven_replacer_plugin.file.FileUtils;
+
+public class OutputFilenameBuilder {
+	
+	private final FileUtils fileUtils;
+
+	public OutputFilenameBuilder() {
+		fileUtils = new FileUtils();
+	}
+
+	public String buildFrom(String inputFilename, ReplacerMojo mojo) {
+		if (mojo.getOutputDir() != null) {
+			String result = mojo.isPreserveDir() ? inputFilename : getJustFilename(inputFilename);
+			if (mojo.getOutputBasedir() != null) {
+				return fileUtils.createFullPath(mojo.getOutputBasedir(), mojo.getOutputDir(), result);
+			}
+			return fileUtils.createFullPath(mojo.getBasedir(), mojo.getOutputDir(), result);
+		}
+		
+		if (mojo.getOutputFile() != null) {
+			return fileUtils.createFullPath(mojo.getBasedir(), mojo.getOutputFile());
+		}
+		return fileUtils.createFullPath(mojo.getBasedir(), inputFilename);
+	}
+
+	private String getJustFilename(String inputFilename) {
+		return new File(inputFilename).getName();
+	}
+
+}
