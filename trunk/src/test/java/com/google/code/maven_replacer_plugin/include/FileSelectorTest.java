0eaf2af493e377b1dcea80c76b10ccfe8ea11f5f
baker.steven.83@gmail.com <baker.steven.83@gmail.com@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2011-11-23 05:10:52 +0000

    Moving test files into proper test dir.

--- trunk/src/test/java/com/google/code/maven_replacer_plugin/include/FileSelectorTest.java
+++ trunk/src/test/java/com/google/code/maven_replacer_plugin/include/FileSelectorTest.java
@@ -16,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FileSelectorTest {
+	private static final String BASE_DIR = "src/test/resources/files";
 	private static final String TEST_FILE = "maven-replacer-plugin-test-file";
 	private static final String BACK_DIR_SYMBOL = "..";
 	
@@ -28,28 +29,28 @@ public class FileSelectorTest {
 	
 	@Test
 	public void shouldReturnMultipleFilesToInclude() {
-		List<String> files = selector.listIncludes("test", asList("include1", "file*"), asList("file3"));
+		List<String> files = selector.listIncludes(BASE_DIR, asList("include1", "file*"), asList("file3"));
 		assertThat(files.size(), is(3));
 		assertThat(files, equalTo(asList("file1", "file2", "include1")));
 	}
 	
 	@Test
 	public void shouldSupportNoExcludes() {
-		List<String> files = selector.listIncludes("test", asList("include1", "file*"), null);
+		List<String> files = selector.listIncludes(BASE_DIR, asList("include1", "file*"), null);
 		assertThat(files, equalTo(asList("file1", "file2", "file3", "include1")));
 	}
 	
 	@Test
 	public void shouldReturnEmptyListWhenEmptyIncludes() {
-		assertTrue(selector.listIncludes("test", null, asList("file3")).isEmpty());
-		assertTrue(selector.listIncludes("test", Collections.<String>emptyList(), asList("file3")).isEmpty());
+		assertTrue(selector.listIncludes(BASE_DIR, null, asList("file3")).isEmpty());
+		assertTrue(selector.listIncludes(BASE_DIR, Collections.<String>emptyList(), asList("file3")).isEmpty());
 	}
 	
 	@Test
 	public void shouldSelectFilesInBackDirectories() throws IOException {
 		File file = new File(BACK_DIR_SYMBOL + File.separator + TEST_FILE);
 		file.deleteOnExit();
-		FileUtils.writeStringToFile(file, "test");
+		FileUtils.writeStringToFile(file, BASE_DIR);
 		
 		List<String> files = selector.listIncludes(BACK_DIR_SYMBOL, asList(TEST_FILE), null);
 		assertThat(files, equalTo(asList(TEST_FILE)));
