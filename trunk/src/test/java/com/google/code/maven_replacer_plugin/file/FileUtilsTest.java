22c13ca3cb6dae55c53c333fde869f76511625f7
baker.steven.83 <baker.steven.83@8e8f2cb0-ffa3-11dd-a320-db7d8e7a638f>
2013-11-07 09:32:18 +0000

    more information if dir creation fails

--- trunk/src/test/java/com/google/code/maven_replacer_plugin/file/FileUtilsTest.java
+++ trunk/src/test/java/com/google/code/maven_replacer_plugin/file/FileUtilsTest.java
@@ -5,7 +5,6 @@ import static org.apache.commons.lang.StringUtils.join;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -119,7 +118,7 @@ public class FileUtilsTest {
 			fileUtils.ensureFolderStructureExists("/f*\"%e$d/a%*bc$:\\te\"st");
 			fail("Should have thrown Error");
 		} catch (IllegalStateException e) {
-			assertEquals(e.getMessage(), "Error creating directory.");
+			assertThat(e.getMessage(), equalTo("Error creating directory: \\f*\"%e$d\\a%*bc$:"));
 		}
 	}
 	
